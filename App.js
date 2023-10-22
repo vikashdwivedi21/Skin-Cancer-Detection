7c8fae5c7e1e4738518e61fcc6fb8946549ec329
medinipatil <85755524+medinipatil@users.noreply.github.com>
2023-10-22 09:30:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import Sidebar from './Components/SideBar/SideBar.jsx'
+import Body from './Components/BodySection/Body.jsx'
+function App() {
+  return(
+
+    <>
+    <div className='container'>
+     <Sidebar/>
+     <Body/>
+   </div>
+ 
+ </>
+
+  )
+
+   
+
+  
+  
+   
+  
+}
+
+export default App;
